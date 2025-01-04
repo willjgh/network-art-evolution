fef6d56f8ebb8aaf78c870df26bbfa855dbb7c67
willjgh <williamjgh2000@gmail.com>
2025-01-04 21:47:33 +0000

    Setup picbreeder style evolution of fixed structure networks

---
+++ fixed-network-evolution/evolution.py
@@ -0,0 +1,29 @@
+from population import Population
+from display import Display
+
+num = 4
+
+# create a population
+population = Population(size=num**2)
+population.initialize()
+
+# create a display
+display = Display(grid_size=num)
+
+while display.running:
+
+    display.event_handler()
+
+    display.draw(population.population)
+
+    # user action prompts new generation
+    if display.action:
+
+        # evolve new population
+        population.evolve(selected=display.selected)
+
+        # reset flag
+        display.action = False
+
+        # reset selection
+        display.selected = []
